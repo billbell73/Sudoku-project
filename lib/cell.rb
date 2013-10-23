18dffda33ad38ead956759a4786bcde7947b4631
Will Bell <willbell@gmail.com>
2013-10-23 21:38:37 +0100

    Added candidates method, mid clean-up

--- lib/cell.rb
+++ lib/cell.rb
@@ -11,19 +11,28 @@ class Cell
 	end
 
 	def candidates grid_object
-		grid = get_grid_for grid_object
-		[]
+		grid = get_grid_from grid_object
+		neighbours_values = get_values_of_neighbours_from grid
+		(1..9).to_a - neighbours_values
 	end
 
-	def neighbours grid
+	def get_grid_from grid_object
+		grid_object.cells
+	end
+
+	def get_values_of_neighbours_from grid
 		neighbours_values = []
 		neighbours_values.concat(values_in_same_row(grid))     
 		neighbours_values.concat(values_in_same_column(grid))
 		neighbours_values.concat(values_in_same_square(grid))
+		clean_up neighbours_values
 	end
 
-	def get_grid_for grid_object
-		grid_object.cells
+#not sure this method is necessary - line 16 seems to do job anyway
+	def clean_up neighbours_values
+		neighbours_values.delete(0)
+		neighbours_values.uniq!
+		neighbours_values
 	end
 
 	def values_in_same_row grid
@@ -32,6 +41,30 @@ class Cell
 
 	def values_in_same_column grid
 		grid.transpose[@x_coordinate]
+	end
+
+	def sudoku_square_number_of_cell
+		(@x_coordinate/3) + ((@y_coordinate/3)*3)
+	end
+
+	def sudoku_square_number_of y_coordinate, x_coordinate
+		(x_coordinate/3) + ((y_coordinate/3)*3)
+	end
+
+	def indices_of_cells_in_same_square
+		indices_of_square_mates = []
+		(0..8).each do | y_coordinate |
+			(0..8).each do | x_coordinate |
+				if sudoku_square_number_of(y_coordinate, x_coordinate) == sudoku_square_number_of_cell
+					indices_of_square_mates << [y_coordinate, x_coordinate]
+				end
+			end
+		end
+		indices_of_square_mates
+	end
+
+	def value_at_indices y_coordinate, x_coordinate, grid
+		grid[y_coordinate][x_coordinate]
 	end
 
 	def values_in_same_square grid
@@ -44,30 +77,4 @@ class Cell
 		values_in_same_square
 	end
 	
-	def value_at_indices y_coord, x_coord, grid
-		grid[y_coord][x_coord]
-	end
-
-
-	def indices_of_cells_in_same_square
-		indices_of_square_mates = []
-		(0..8).each do | y_coordinate |
-			(0..8).each do | x_coordinate |
-				if square_number_of(y_coordinate, x_coordinate) == square_number_of_cell
-					indices_of_square_mates << [y_coordinate, x_coordinate]
-				end
-			end
-		end
-		indices_of_square_mates
-	end
-
-	def square_number_of_cell
-		(@x_coordinate/3) + ((@y_coordinate/3)*3)
-	end
-
-	def square_number_of y_coordinate, x_coordinate
-		(x_coordinate/3) + ((y_coordinate/3)*3)
-	end
-
-
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -16,12 +16,12 @@ describe Cell do
 
 	xit 'can tell it\'s neighbours' do
 		grid = double :grid, cells: [2,3,4], transpose: [[2],[3],[4]]
-		expect(cell.neighbours grid).to be_instance_of Array
+		expect(cell.get_values_of_neighbours_from grid).to be_instance_of Array
 	end
 
 	it 'can get the full grid' do
 		expect(grid).to receive(:cells)
-		cell.get_grid_for grid
+		cell.get_grid_from grid
 	end
 
 	it 'returns the values of cells in the same row' do
@@ -35,21 +35,21 @@ describe Cell do
 	end
 
 	it 'can tell its square number if it is at index [0][0]' do
-		expect(cell.square_number_of_cell).to eq 0
+		expect(cell.sudoku_square_number_of_cell).to eq 0
 	end
 
 	it 'can tell its square number if it is at index [7][1]' do
 		cell = Cell.new(0, 7, 1)
-		expect(cell.square_number_of_cell).to eq 6
+		expect(cell.sudoku_square_number_of_cell).to eq 6
 	end
 
 	it 'can tell its square number if it is at index [5][8]' do
 		cell = Cell.new(0, 5, 8)
-		expect(cell.square_number_of_cell).to eq 5
+		expect(cell.sudoku_square_number_of_cell).to eq 5
 	end
 
 	it 'can tell square number for given x and y co-ordinates' do
-		expect(cell.square_number_of 5, 3).to eq 4
+		expect(cell.sudoku_square_number_of 5, 3).to eq 4
 	end
 
 	it 'returns indices of cells in same square' do
@@ -74,31 +74,39 @@ describe Cell do
 		expect(cell.values_in_same_square grid).to eq [0, 0, 1, 2, 3, 4, 5, 0, 0]
 	end
 
-	it 'return the values of cells in same square, different example' do
+	it 'returns the values of cells in same square, different example' do
 		cell = Cell.new(7,2,8)
 		grid = [[0,0,1,9,3,4,7,7,7],[2,3,4,9,3,4,7,7,7],[5,0,0,9,3,4,7,7,7]]
 		expect(cell.values_in_same_square grid).to eq [7, 7, 7, 7, 7, 7, 7, 7, 7]
 	end
 
+	it 'can tell it\'s neighbours include 5' do
+		grid = [[0,0,1],[2,3,4],[5,0,0]]
+		expect(cell.get_values_of_neighbours_from(grid).include?(5)).to be_true
+	end
 
+	it 'can tell candidate values' do
+		grid_object = double :grid, cells: [[0,0,1,9],[2,3,4,7],[5,0,0,8]]
+		expect(cell.candidates grid_object).to eq [6,7,8]
+	end
 
+	it 'can tell candidate values, different example' do
+		cell = Cell.new(0,2,0)
+		grid_object = double :grid, cells: [[5,3,2],[0,0,1],[0,6,0],[3,7,8]]
+		expect(cell.candidates grid_object).to eq [4,7,8,9]
+	end
 
-
-
-
-
-
+	it 'cleans up array by removing zeros and duplicates' do
+		values = [0,0,0,6,6,7,8,8]
+		expect(cell.clean_up values).to eq [6,7,8]
+	end
 
 ###
 
-	# it 'test on neighbours method' do
+	# it 'test on get_values_of_neighbours_from method' do
 	# 	grid_object = double :grid, cells: [[0,0,1],[2,3,4],[5,0,0]]
-	# 	expect(cell.neighbours grid_object).to eq [[0,0,1],[0,2,5]]
+	# 	expect(cell.get_values_of_neighbours_from grid_object).to eq [[0,0,1],[0,2,5]]
 	# end
 
-	it 'can tell it\'s neighbours include 5' do
-		grid = [[0,0,1],[2,3,4],[5,0,0]]
-		expect(cell.neighbours(grid).include?(5)).to be_true
-	end
-
+	
 end
