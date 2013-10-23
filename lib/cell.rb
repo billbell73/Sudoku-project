b33b04cd5037f3c63bf1c533fe215a1beb14edf5
Will Bell <willbell@gmail.com>
2013-10-23 11:38:05 +0100

    implemented values_in_same_row method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,22 +1,39 @@
 class Cell
 
-	def initialize
-		@value = nil
+	def initialize(value, row, column)
+		@value = value
+		@row = row
+		@column = column
 	end
 
 	def filled_out?
-		0
+		@value
 	end
 
 	def candidates
 		[]
 	end
 
-	def neighbours grid
-		grid.cells
-		[]
+	def neighbours grid_object
+		full_grid = get_grid grid_object
+		#logic which iterates through array choosing cells in same array and cells at same index in other array
+		neighbours_values = []
+		neighbours_values << (values_in_same_row(full_grid))        #full_grid[@row]
 	end
 
+	def get_grid grid_object
+		grid_object.cells
+	end
+
+	# #pass full_grid and POSITION?? and works out neighbours
+	# def neighbour_finder full_grid
+	# 	# neighbours_values = []
+	# 	# neighbours_values << full_grid[0]
+	# end
+
+	def values_in_same_row full_grid
+		full_grid[@row]
+	end
 
 
 
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,11 +2,11 @@ require 'cell'
 
 
 describe Cell do
-	let(:cell) { Cell.new }
+	let(:cell) { Cell.new(0,0,0) }
 	let(:grid) {double :grid}
 
 	it 'knows if it\'s filled out' do
-		expect(cell.filled_out?).to be_instance_of Fixnum
+		expect(cell.filled_out?).to be_true
 	end
 
 	it 'can tell candidate numbers when empty' do
@@ -18,11 +18,24 @@ describe Cell do
 		expect(cell.neighbours grid).to be_instance_of Array
 	end
 
-	it 'can tell it\s neighbours include 5' do
+	it 'can get the full grid' do
 		expect(grid).to receive(:cells)
-		cell.neighbours grid
+		cell.get_grid grid
 	end
 
+	xit 'can tell it\s neighbours include 5' do
 
+		# expect(neighbour_finder grid).to return Array
+	end
+
+	it 'returns the values of cells in the same row' do
+		full_grid = [[0,0,1],[2,3,4],[5,0,0]]
+		expect(cell.values_in_same_row full_grid).to eq [0,0,1]
+	end
+
+	it 'test on neighbours method' do
+		grid_object = double :grid, cells: [[0,0,1],[2,3,4],[5,0,0]]
+		expect(cell.neighbours grid_object).to eq [[0,0,1]]
+	end
 
 end
